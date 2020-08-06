2943e76af1b8f943406b0630912ca6eb1f5b394c
Stefan Dumitrescu <dumitrescu.stefan@gmail.com>
2020-08-06 13:39:58 +0300

    Update v1.1

--- a.py
+++
@@ -1,9 +0,0 @@
-import rowordnet as rwn
-rwn = rwn.RoWordNet()
-
-a = rwn.synsets("cal")
-
-a = rwn.synset('ENG30-03624767-n')
-b = rwn.synset('ENG30-07666406-n')
-
-print(rwn.wup_similarity(a.id,a.id))
